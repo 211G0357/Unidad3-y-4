47922dccbb2f7faf2c360ce99e22b4a8f64aebc1
211G0357 <211G0357@rcarbonifera.tecnm.mx>
2025-06-18 14:46:41 -0600

    ya

---
+++ RestauranteApi/Models/DTOs/ActualizarEstadoDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestauranteApi.Models.DTOs
+{
+    public class ActualizarEstadoDTO
+    {
+        [Required]
+        public string Estado { get; set; }
+    }
+}
